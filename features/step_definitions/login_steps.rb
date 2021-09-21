7336eeca0411081755e8aae105d173b0b6cf87e2
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-09-20 20:39:07 -0600

    first commit

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,19 @@
+Given(/^the user navigate to the homepage$/) do
+  pending
+end
+
+And(/^user clicks form Authentication$/) do
+  pending
+end
+
+And(/^user enters valid "([^"]*)" and valid "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+And(/^User clicks Login Button$/) do
+  pending
+end
+
+Then(/^verify user is successfully login with a message "([^"]*)"$/) do |arg|
+  pending
+end
